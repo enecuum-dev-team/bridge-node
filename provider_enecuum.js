1042e6f358fc8ee0e83dff229fda300456c66c70
wWrongName <name.johnny@mail.ru>
2023-07-25 13:30:11 +0300

    hotfix: add net and add validator's crutch

--- provider_enecuum.js
+++ provider_enecuum.js
@@ -81,16 +81,16 @@ let http_post = function (url, json) {
 
 let config = {
     contract_pricelist: {
-        "create_token" :    20000000000,
-        "create_pos" :      0,
-        "delegate" :        0,
-        "undelegate" :      0,
-        "transfer" :        0,
-        "pos_reward" :      0,
-        "mint" :            0,
-        "burn" :            0,
-        "custom" :          20000000000,
-        "pool_create" :     0,
+        "create_token" :            20000000000,
+        "create_pos" :              0,
+        "delegate" :                0,
+        "undelegate" :              0,
+        "transfer" :                0,
+        "pos_reward" :              0,
+        "mint" :                    0,
+        "burn" :                    0,
+        "custom" :                  20000000000,
+        "pool_create" :             0,
         "pool_add_liquidity" :      0,
         "pool_remove_liquidity" :   0,
         "pool_sell_exact" :         0,
@@ -104,10 +104,15 @@ let config = {
         "pool_sell_exact_routed" :  0,
         "pool_buy_exact" :          0,
         "pool_buy_exact_routed" :   0,
-        "lock" :                    0,
-        "claim_init" :              0,
-        "claim_confirm" :           0,
-        "claim" :                   0
+        "bridge_lock" :             0,
+        "bridge_claim_init" :       0,
+        "bridge_claim_confirm" :    0,
+        "bridge_set_owner" :        0,
+        "bridge_set_threshold" :    0,
+        "bridge_add_validator" :    0,
+        "bridge_remove_validator" : 0,
+        "bridge_add_network" :      0,
+        "bridge_remove_network" :   0
     },
 };
 
@@ -457,7 +462,8 @@ module.exports = class EnecuumNetwork extends Network {
             tx_info = JSON.parse(tx_info);
 
             let parser = new ContractParser(config);
-
+            if (tx_info.data.indexOf("undefined"))
+                throw new Error("invalid data")
             let params = parser.parse(tx_info.data);
 
             console.trace(JSON.stringify(params));
